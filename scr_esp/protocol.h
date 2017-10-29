9824419d1a0befb5bc1936a8549711c65ae5169e
nghiaphung <ducnghia318@gmail.com>
2017-10-29 23:15:40 +0700

    scr ESP8266 and arduino Uno. origin source code of esp is wisock

---
+++ scr_esp/protocol.h
@@ -0,0 +1,24 @@
+#ifndef __PROTOCOL_H__
+#define __PROTOCOL_H__
+
+/***************************************************************************************
+* INCLUDE
+***************************************************************************************/
+#include "wishock.h"
+
+/***************************************************************************************
+* PRIVATE DEFINES
+***************************************************************************************/
+#define  PROCESS_NORMAL  0
+#define  FRAME_ERR       1
+#define  PROCESS_ERR     2
+
+/***************************************************************************************
+* PUBLIC FUNCTION PROTOTYPES
+***************************************************************************************/
+void protocolInit(void);
+//int pars_json(String json);
+void protocolDataProcess (String recv_json);
+void protocolButtonProcess (void);
+int protocolSerialRecv (String *s);
+#endif
